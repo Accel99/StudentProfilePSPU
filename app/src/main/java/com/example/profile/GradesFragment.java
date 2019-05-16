87fced9fc0659614904d0ed8fa68cd25f59cf6d6
Accel99 <anpilogov.sasha@gmail.com>
2019-05-16 16:23:51 +0500

    Create Child Fragments for Grades

--- app/src/main/java/com/example/profile/GradesFragment.java
+++ app/src/main/java/com/example/profile/GradesFragment.java
@@ -2,17 +2,24 @@ package com.example.profile;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GradesFragment extends Fragment {
+public class GradesFragment extends Fragment implements View.OnClickListener {
 
+    Button btnSession;
+    Button btnSemesters;
 
     public GradesFragment() {
         // Required empty public constructor
@@ -23,7 +30,43 @@ public class GradesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ((MainActivity)getActivity()).setActionBarTitle(getResources().getString(R.string.activity_main_drawer_grades));
-        return inflater.inflate(R.layout.fragment_grades, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_grades, container, false);
+
+        btnSession = (Button) v.findViewById(R.id.btnSession);
+        btnSemesters = (Button) v.findViewById(R.id.btnSemesters);
+
+        btnSession.setOnClickListener(this);
+        btnSemesters.setOnClickListener(this);
+
+        return v;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+        ft.replace(R.id.chaildclGrades, new SessionChildFragment());
+        ft.commit();
+        btnSession.setBackgroundResource(R.color.colorPrimary);
+        btnSemesters.setBackgroundResource(R.color.colorGray);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if (id == R.id.btnSession) {
+            FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+            ft.replace(R.id.chaildclGrades, new SessionChildFragment());
+            ft.commit();
+            btnSession.setBackgroundResource(R.color.colorPrimary);
+            btnSemesters.setBackgroundResource(R.color.colorGray);
+        } else if (id == R.id.btnSemesters) {
+            FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+            ft.replace(R.id.chaildclGrades, new SemestersChildFragment());
+            ft.commit();
+            btnSemesters.setBackgroundResource(R.color.colorPrimary);
+            btnSession.setBackgroundResource(R.color.colorGray);
+        }
+    }
 }
